9d8e76d69c83c380cde9babd6ac23f2802d189ff
Anay Shah <anayshah10@gmail.com>
2026-03-22 02:29:40 +0530

    added .next to gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e9026a6ca2f474f8"; }
+/******/ 		__webpack_require__.h = function() { return "50ce5de70c79dee8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
